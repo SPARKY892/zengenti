98d2e5a93e4963726a8fcc08a5b7b09cbd9eff3b
Mark <marksmellie892@gmail.com>
2021-05-16 12:36:09 +0100

    Updated styling and positioning

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './Banner.scss';
-import {ReactComponent as BannerImage} from '../images/banner-image.svg';
+import BannerImage from '../images/banner-image.svg';
 
-const Hero = () => {
+const Banner = () => {
     return(
         <React.Fragment>
             <div className="banner-container">
-                <div className="hero-sub-container">
+                <img src={BannerImage} alt="Banner" />
+                <div className="banner-sub-container">
                     <div className="banner-marker" />
-                    <h3 className="hero-text">There is so much going on at Ludlow University, both on and off campus.</h3>
-                    <button className="hero-btn">View student stories</button>
+                    <h3 className="banner-text">Come along to one of our Open Days and find out everything you need to know about the campus, courses and facilities</h3>
+                    <button className="banner-btn">View student stories</button>
                 </div>
             </div>
         </React.Fragment>
     );
 };
 
-export default Hero;
+export default Banner;
