1237a4739916107413ff3e5b9aafc6d1314980ab
Mark <marksmellie892@gmail.com>
2021-05-16 13:57:19 +0100

    Updating footer styling

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
         <Cards />
         <Banner />
         <Upcoming />
+        <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,31 @@
 import React from 'react';
+import TwitterIcon from '../images/twitter.svg';
+import FacebookIcon from '../images/facebook.svg';
+import LinkedInIcon from '../images/linkedin.svg';
+import GoogleIcon from '../images/google.svg';
+import FadedLogo from '../images/faded-logo.svg';
+import Logo from '../images/logo.svg';
 import './Footer.scss';
 
 const Footer = () => {
     return (
         <footer className="footer-container">
-            <img className="footer-logo" src="https://cdn.shortpixel.ai/client/q_glossy,ret_img/https://dycoit.co.uk/wp-content/uploads/2019/02/dyco-logo-x75.png" />
-            <p className="footer-text">We will customize and design our business support services for your business needs and structure.</p>
-            <div className="footer-icon-container">
-                <a className="footer-icon-link" href="#">
-                    <i className="footer-icon fab fa-instagram fa-2x"></i>
-                </a>
-                <a className="footer-icon-link" href="#">
-                    <i className="footer-icon fab fa-linkedin-in fa-2x"></i>
-                </a>
-                <a className="footer-icon-link" href="#">
-                    <i className="footer-ico fab fa-facebook fa-2x"></i>
-                </a>
-            </div>
-            <div className="footer-services-container">
-                <h4 className="footer-header">Our Services</h4>
-                <a className="footer-service-link" href="#"></a>
-                <a className="footer-service-link" href="#"></a>
-                <a className="footer-service-link" href="#"></a>
-                <a className="footer-service-link" href="#"></a>
-                <a className="footer-service-link" href="#"></a>
-                <a className="footer-service-link" href="#"></a>
-                <a className="footer-service-link" href="#"></a>
-            </div>
-            <div className="footer-contact-container">
-                <h4 className="footer-header">Contact Info</h4>
+            <div className="footer-sub-container">
+                <div className="footer-faded-logo-container">
+                    <img src={FadedLogo} className="footer-faded-logo" alt="Faded Logo" />
+                    <div className="footer-logo-social-container">
+                        <img className="footer-logo" src={Logo} />
+                        <div className="footer-social-container">
+                            <img src={TwitterIcon} alt="Twitter" />
+                            <img src={LinkedInIcon} alt="LinkedIn" />
+                            <img src={GoogleIcon} alt="Google" />
+                            <img src={FacebookIcon} alt="Facebook" />
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-twitter-container">
 
+                </div>
             </div>
         </footer>
     );
